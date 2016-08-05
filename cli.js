5edae272cf8ba3131a683e7acdd0a42682c5f39f
Zach Bray <zachbray@googlemail.com>
2016-08-05 00:38:17 +0000

    Fixed another bug with nib usage.

--- cli.js
+++ cli.js
@@ -33,7 +33,7 @@ var writeFile = function(f) {
       return;
     }
     console.log('Rendering ' + f);
-    stylus(source).use(nib).set('filename', f).render(function(err, css) {
+    stylus(source).use(nib()).set('filename', f).render(function(err, css) {
       if (err) {
         console.log('[Error] ' + err);
         return;
